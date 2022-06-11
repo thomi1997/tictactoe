ad03dc4d3d20f8a8f36924a9f77f2edd6c3d4723
unknown <thomas_ketler@web.de>
2022-06-11 22:43:28 +0200

    refresh tictactoe

--- script.js
+++ script.js
@@ -3,24 +3,72 @@ let currentshape = 'cross';
 
 
 function fillShape(id) {
-    if (currentshape == 'cross') {
-        currentshape = 'circle';
-    }else {
-        currentshape = 'cross';
+    if (!fields[id]) {
+        if (currentshape == 'cross') {
+            currentshape = 'circle';
+            document.getElementById('player-1').classList.remove('player-inactive');
+            document.getElementById('player-2').classList.add('player-inactive');
+        } else {
+            currentshape = 'cross';
+            document.getElementById('player-1').classList.add('player-inactive');
+            document.getElementById('player-2').classList.remove('player-inactive');
+        }
+        fields[id] = currentshape;
+        console.log(currentshape);
+        draw();
+        checkForWin();
     }
-    fields[id] = currentshape;
-    console.log(currentshape);
-    draw();
 }
 
 function draw() {
-    for(let i=0; i < fields.length; i++) {
-        if(fields[i] == 'circle') {
+    for (let i = 0; i < fields.length; i++) {
+        if (fields[i] == 'circle') {
             document.getElementById('circle-' + i).classList.remove('d-none');
         }
 
-        if(fields[i] == 'cross') {
+        if (fields[i] == 'cross') {
             document.getElementById('cross-' + i).classList.remove('d-none');
         }
+    }
+}
+
+
+function checkForWin() {
+    let winner;
+
+    if (fields[0] == fields[1] && fields[1] == fields[2] && fields[0]) {
+        winner = fields[0];
+    }
+
+    if (fields[3] == fields[4] && fields[4] == fields[5] && fields[3]) {
+        winner = fields[3];
+    }
+
+    if (fields[6] == fields[7] && fields[7] == fields[8] && fields[6]) {
+        winner = fields[6];
+    }
+
+    if (fields[0] == fields[3] && fields[3] == fields[6] && fields[0]) {
+        winner = fields[0];
+    }
+
+    if (fields[1] == fields[4] && fields[4] == fields[7] && fields[1]) {
+        winner = fields[1];
+    }
+
+    if (fields[2] == fields[5] && fields[5] == fields[8] && fields[2]) {
+        winner = fields[2];
+    }
+
+    if (fields[0] == fields[4] && fields[4] == fields[8] && fields[0]) {
+        winner = fields[0];
+    }
+
+    if (fields[2] == fields[4] && fields[4] == fields[6] && fields[2]) {
+        winner = fields[2];
+    }
+
+    if (winner) {
+        console.log('GEWONNEN', winner);
     }
 }
